da71c72bc9065457599db014add3aa497a806c71
Thomas Harris <lostmt.tom@gmail.com>
2018-08-31 11:36:11 -0700

    docs: Adds comments to cache files

--- js/sw.js
+++ js/sw.js
@@ -1,5 +1,5 @@
-self.addEventListener('install', function(event) {
-  event.waitUntil(
+self.addEventListener('install', function(event) { //the goal of this file is to call cached files
+  event.waitUntil( //The code here was taken from https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
     caches.open('v1').then(function(cache) {
       return cache.addAll([
         '/index.html',
